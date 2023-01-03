50501d3555e825ccec8b032ac609026dce0f9318
Mario Barberis <mariojbarberis@hotmail.com>
2023-01-02 22:34:32 -0300

    TPfinal vol. 3

--- TPFinal/servicios.php
+++ TPFinal/servicios.php
@@ -6,7 +6,27 @@ include('navegador.php');
 
 <section class="serv_contenido">
 
-    <h1>SERVICIOS DISPONIBLES EN NUESTRO RETIRO DE DESCANSO:</h1>
+    <p class="parrafo">Los servicios de belleza son todas aquellas actividades que se realizan con el fin de modificar temporalmente la apariencia estética del cuerpo humano a nivel del cabello, cuero cabelludo y uñas.
+
+El texto objeto de análisis, Servicio de belleza, publicado por la Editorial Pueblo y Educación en el año 2019, viene a llenar un vacío bibliográfico dentro de la disciplina Servicios de belleza de la Enseñanza Técnico Profesional (ETP).
+
+La autora Marelis Proenza Brown, Guantánamo, 1959), es Licenciada en Economía, estilista profesional con más de 30 años de experiencia en la profesión, autora de los libros como: El Arte de la Peluquería, 2013 por la editorial Oriente, Recetas Caseras de Belleza por la editorial El Mar y la Montaña. 2016, Aprenda a Maquillarse 2017 por la editorial Oriente. El Arte de la Peluquería. Segunda edición, por la editorial El Mar y la Montaña. Publicó un artículo titulado: Aportes del cuentapropista al desarrollo local, partiendo del conocimiento para crear capacidades y competencia, en la multimedia interactiva Memorias Tecnogest 2015. Se graduó de Especialista Universitario en Cosmética y Dermofarmacia de la Universidad Miguel de Cervantes de España y Especialista en Medicina Estética: Tratamientos Faciales de Euroinnova un centro de diplomados de España. Es una activa promotora de temas de belleza en la radio y en la televisión. Imparte cursos en la Casa de Orientación a la Mujer y la Familia de su provincia.
+
+Su libro Servicio de belleza es de vital importancia para todos los miembros de la sociedad cubana, es una bibliografía de consulta para dar tratamiento a este tema en las diferentes instituciones de este tipo de enseñanza (ETP) e incluso para uso personal.
+
+La escritora, partiendo de sus experiencias personales, y en coordinación con el Ministerio de Educación, fundamentalmente la Enseñanza Técnico Profesional (ETP), logró compendiar sus libros anteriores surgiendo así Servicio de belleza donde muestra cómo preparar a los jóvenes para participar en el mercado laboral actual. Es un material de consulta para los servicios en la especialidad del mismo nombre.
+
+En él se muestran ejemplos sencillos, claros y concretos de cómo despertar y mantener el interés del cliente entre otros temas y contribuir así al mejoramiento de habilidades comunicativas en la prestación de servicios sin apartarse de las condiciones actuales de nuestro país. Por tanto, la aplicación práctica de sus mensajes facilitará el logro de una prestación de servicio eficiente, con todo el respeto que nuestro pueblo merece.
+
+El texto revela claridad, coherencia, y concisión en sus temas. Cuenta con una introducción donde se hace un recuento histórico de la Cosmetología en Egipto, Grecia, Roma, los pueblos bárbaros, La Edad Media, El Renacimiento, El Barroco, Siglo xix y xx, y por último realiza una breve reseña de la peluquería en Cuba: su surgimiento y desarrollo.
+
+Está estructurado en catorce capítulos abordando temas como la peluquería y la imagen del profesional; normas de seguridad e higiene de peluqueros y barberos; el cabello su lavado, corte, selección del champú, acondicionadores y tratamiento del mismo; maquillaje, cuidado de la piel y el rostro; brinda algunas recetas naturales para la piel y el rostro así como para el cuidado del cabello; ofrece una relación de plantas medicinales con sus nombre común y científico; brinda información sobre las herramientas y accesorios de trabajo para desempeñar el oficio.
+
+La comercialización de los libros anteriores que fueron la premisa para esta nueva obra fue significativa y decisoria, lo cual refleja su importancia y aceptación por parte de toda la población deseosa por conocer sobre estos temas que, aunque son muy útiles, precisan de un manejo responsable y profesional para cuidar la apariencia personal de todos. Culmino el análisis del texto con una frase que identifica a la autora:
+
+“La belleza complace a los ojos, enaltece el alma y conquista el corazón”</p>
+
+<h1 class="tituloserv">SERVICIOS DISPONIBLES EN NUESTRO RETIRO DE DESCANSO:</h1>
 
     <ul>
         <li> <a href="servicios.php?servicios=masajes">Masajes</a></li>
@@ -15,7 +35,6 @@ include('navegador.php');
     </ul>
 
 </section>
-
 
 <?php
 
